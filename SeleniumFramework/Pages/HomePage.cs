055bf20c7586b829831c8ac82b99981fe980ada0
ngothikimthaoqb <ngothikimthaoqb@gmail.com>
2022-11-17 11:19:46 +0700

    Update Homepage

--- SeleniumFramework/Pages/HomePage.cs
+++ SeleniumFramework/Pages/HomePage.cs
@@ -12,7 +12,7 @@ namespace SeleniumFramework.Pages
         private IWebElement SignInLink { get; set; }
 
         private static string HomePageTitle = "Oxford Leaner's Dictionaries";
-        private static string SignInPageTitle = "Sign in | Oxford University Press ";
+        private static string SignInPageTitle = "Sign in | Oxford University Press";
 
         private static Uri HomePageUrl => new Uri(ConfigurationProvider.Environment.ApplicationUrl);
 
@@ -27,14 +27,13 @@ namespace SeleniumFramework.Pages
             Driver.Navigate().GoToUrl(HomePageUrl);
         }
 
-        internal bool IsPageTitleCorrect() => Driver.Title.Equals(HomePageTitle + "jdnckdnk");
+        internal bool IsPageTitleCorrect() => Driver.Title.Equals(HomePageTitle);
         internal bool IsSignInPageTitleCorrect() => Driver.Title.Equals(SignInPageTitle);
 
 
         internal void NavigartionToLoginPage()
         {
             SignInLink.Click();
-            IsSignInPageTitleCorrect();
         }
 
     }
